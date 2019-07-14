f736f829f5079e9ba4fa3d3ca6d9f2b007c867d1
rinakawamura <rina.kawamura@yale.edu>
2019-07-14 18:36:02 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(obj => {
+    obj.result === "W"
+  })
+  if (win) {
+    win.year
+  }else {
+    "undefined"
+  }
+    
+}
