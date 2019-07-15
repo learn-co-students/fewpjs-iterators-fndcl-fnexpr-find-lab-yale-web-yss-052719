1cbfd1ac54dd56638d67f8f381b7c4f68483b7fe
Elijah <efloren@siue.edu>
2019-07-15 13:15:15 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let game = record.find(function(e) {
+    debugger 
+    return e.result === "W"
+   }) 
+   if (game) { return game.year } 
+  }
