689cfa91fdecd5f81f69a0f9a0792b8b05fbd09a
Leonie Dong <leoniedong@hotmail.com>
2019-07-15 01:34:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+function superbowlWin(record) {
+  if (record.find(x => x.result === "W") != undefined) {
+    return record.find(x => x.result === "W").year;
+  } else {
+    return undefined;
+  }
+  
 }
